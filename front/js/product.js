d7a449164a7e699dd513b1a5377d1d9e5fce139a
Issou83 <issou8@gmail.com>
2022-12-18 15:33:00 +0100

    Introduction des elemments du produit dans la page'product'. A faire: les couleurs à rajouter

--- front/js/product.js
+++ front/js/product.js
@@ -11,16 +11,21 @@ fetch("http://localhost:3000/api/products/" + idProduct)
   .then((dataProduct) => dataProduct.json())
   //Récupèration des données
   .then((product) => {
-    //Boucle nous permettant d'extraire chaque element du tableau "products"
     console.log(product);
     console.log(product.name);
     console.log(product.description);
     console.log(product.price);
     console.log(product.imageUrl);
-    
-    //On peut désormais introduir les éléments du produit dans la page
-    //On vise l'emplacement souhaité poue y introduir la bonne variable
 
+    // On peut désormais introduir les éléments du produit dans leurs emplcements respectifs
+    // On vise l'emplacement souhaité pour y introduir la bonne valeur de product
 
-  
+    document.querySelector('.item__img').innerHTML = (`<img src="${product.imageUrl}" alt="${product.altText}">`);
+    document.getElementById("title").innerText = product.name;
+    document.getElementById("description").innerText = product.description;
+    document.getElementById("price").innerText = product.price;
+
   });
+
+
+
