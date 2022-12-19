13985d7f351c126199e43573382dd1079ab92cd9
Issou83 <issou8@gmail.com>
2022-12-19 21:29:39 +0100

    Injection du choix des couleurs dans la page 'product'

--- front/js/product.js
+++ front/js/product.js
@@ -9,6 +9,7 @@ console.log(idProduct);
 fetch("http://localhost:3000/api/products/" + idProduct)
   //Récupération des données du "product" au format json
   .then((dataProduct) => dataProduct.json())
+  
   //Récupèration des données
   .then((product) => {
     console.log(product);
@@ -16,7 +17,7 @@ fetch("http://localhost:3000/api/products/" + idProduct)
     console.log(product.description);
     console.log(product.price);
     console.log(product.imageUrl);
-
+    console.log(product.colors);
     // On peut désormais introduir les éléments du produit dans leurs emplcements respectifs
     // On vise l'emplacement souhaité pour y introduir la bonne valeur de product
 
@@ -24,6 +25,15 @@ fetch("http://localhost:3000/api/products/" + idProduct)
     document.getElementById("title").innerText = product.name;
     document.getElementById("description").innerText = product.description;
     document.getElementById("price").innerText = product.price;
+
+
+    //Pour la couleur on utilise un boucle qui nous permettant d'extraire chaque couleur du tableau "colors"
+    for (let colors of product.colors) {
+        console.log(colors);
+
+    //Injection des éléments de couleur dans le DOM : dans notre id "
+    document.getElementById("colors").innerHTML += (`<option value="${colors}">${colors}</option>`);
+    }
 
   });
 
