dffa251aa9db9f3438ccb1e5b81bdebcadf10311
Issou83 <issou8@gmail.com>
2022-12-22 16:27:38 +0100

    Ajout du choix des couleurs du produit + Enregistrement des informations du produit choisi dans le localStorage à l'aide du click sur bouton 'Ajouter au panier'

--- front/js/product.js
+++ front/js/product.js
@@ -9,7 +9,7 @@ console.log(idProduct);
 fetch("http://localhost:3000/api/products/" + idProduct)
   //Récupération des données du "product" au format json
   .then((dataProduct) => dataProduct.json())
-  
+
   //Récupèration des données
   .then((product) => {
     console.log(product);
@@ -21,42 +21,43 @@ fetch("http://localhost:3000/api/products/" + idProduct)
     // On peut désormais introduir les éléments du produit dans leurs emplcements respectifs
     // On vise l'emplacement souhaité pour y introduir la bonne valeur de product
 
-    document.querySelector('.item__img').innerHTML = (`<img src="${product.imageUrl}" alt="${product.altText}">`);
+    document.querySelector(
+      ".item__img"
+    ).innerHTML = `<img src="${product.imageUrl}" alt="${product.altText}">`;
     document.getElementById("title").innerText = product.name;
     document.getElementById("description").innerText = product.description;
     document.getElementById("price").innerText = product.price;
 
-
     //Pour la couleur on utilise un boucle qui nous permettant d'extraire chaque couleur du tableau "colors"
-    for (let color of product.colors) {
-        console.log(color);
+    for (let i = 0; i < product.colors.length; i++) {
+      console.log(product.colors[i]);
 
-    //Injection des éléments de couleur dans le DOM : dans notre id "
-    // document.getElementById("colors").innerHTML += (`<option value="${colors}">${colors}</option>`);
-    // }
+      //Injection des éléments de couleur dans le DOM, en fonction de notre id de produit"
+
+      const optionColors = document.getElementById("colors");
+      let newsOptions = document.createElement("option");
+      newsOptions.setAttribute(`value`, product.colors[i]);
+
+      newsOptions.innerText = product.colors[i];
+      optionColors.appendChild(newsOptions);
+    }
 
     //Stockage des éléments choisis dans la page 'product' pour futur utilisation dans la page du panier
-    //Variables des elements à pointer
-    const button = document.getElementById('addToCart')
 
-    button.addEventListener(`click`, function() {
-
-
-
-    let objJson = {
-        name: product.name,
+    //Fonction qui nous permet au click du bouton, nous savegardons les infos choisis du produit selectionné
+    document.getElementById("addToCart").addEventListener("click", function () {
+      // localStorage.setItem("id", idProduct);
+      // localStorage.setItem("quantity", quantity.value);
+      // localStorage.setItem("color", colors.value);
+      let objJson = {
+        id: idProduct,
+        quantity: quantity.value,
         color: colors.value,
-        price: product.price 
-    }
-    let objLinea = JSON.stringify(objJson);
-    localStorage.setItem("selectProduct", objLinea);
-
-    })
-  }})
-
-
+      };
+      let objLinea = JSON.stringify(objJson);
+      localStorage.setItem("obj", objLinea);
+    });
+  });
 
 // Clear temporaire pour remise à Zero du tableau "localStorage"
 //   localStorage.clear();
-
-
